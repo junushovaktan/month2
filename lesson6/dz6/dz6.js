867521164cf1690fc48866260a82da5e0dad6e87
junushovaktan <junushovaktan@gmail.com>
2022-03-25 22:18:41 +0600

    domashnya 6

--- lesson6/dz6/dz6.js
+++ lesson6/dz6/dz6.js
@@ -21,8 +21,32 @@ const obj2 = {
     key8: "8"
 }
 console.log(obj2);
+const dv3 = []
+const v = document.querySelector("#inp")
+const bt = document.querySelector("#btn")
 
-btn.onclick = function () {
-     let v = document.getElementById('inp').value;
-     document.getElementById('al').innerHTML = 'Салам Попалам  '+ v + ' :))';
+function render() {
+    const div = document.createElement('div');
+    div.setAttribute("class", "dv3");
+    for(let i = 0; i < dv3.length; i++ ) {
+        const divList = document.createElement('div');
+        divList.setAttribute("class", "doing");
+        const p = document.createElement("p");
+        p.innerHTML = dv3[i].text;
+        divList.append(p);
+        div.append(divList);
+    }
+    const form = document.querySelector(".dv1");
+    document.querySelector(".dv3").remove();
+    form.append(div)
 }
+
+bt.onclick = function() {
+    const obj = {
+        id: dv3.length,
+        text: "Салам попалам " + v.value + " :)"
+    }
+    this.append(dv3);
+    dv3.push(obj);
+    render()
+};
